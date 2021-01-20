103598b04a0ed646c3b03adf364937870f5e382d
Berkan Senger <sengerberkann@gmail.com>
2021-01-20 23:50:46 +0300

    20 Ocak Ders sonu

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun) //Büyük ürün tipi
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokadeti)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi));
+        }
+    }
+}
